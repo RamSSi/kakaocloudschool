0433f6109f27e51bc93f6f29243a2dd29b929deb
RamSSi <001229aram@gmail.com>
2022-08-03 22:09:42 +0900

    2022-08-03

--- 연습/index.js
+++ 연습/index.js
@@ -11,10 +11,37 @@
 
 
 
-// Array.prototype.filter()
-// : 주어진 함수의 조건을 통과하는 모든 요소를 모아 새로운 배열로 반환
+// // Array.prototype.filter()
+// // : 주어진 함수의 조건을 통과하는 모든 요소를 모아 새로운 배열로 반환
 
-const words = ['세종대왕', '이황', '강감찬', '샤인머스켓', "소리없는 아우성"];
-words.filter((word) => {
-    return word.length > 3;
-})
+// const words = ['세종대왕', '이황', '강감찬', '샤인머스켓', "소리없는 아우성"];
+// words.filter((word) => {
+//     return word.length > 3;
+// })
+
+
+
+// 전개 연산자 (Spread Operator)
+
+let arr1 = [1, 2, 3];
+let arr2 = [4, 5, 6];
+let arr3 = [7, 8, 9];
+
+// let result = arr1.concat(arr2, arr3);
+// console.log(result);    // Array(9) : [1, 2, 3, 4, 5, 6, 7, 8 ,9]
+
+let result = [...arr1, ...arr2, ...arr3];
+console.log(result);    // [1, 2, 3, 4, 5, 6, 7, 8, 9]
+
+let obj1 = {
+    a: '1',
+    b: '2'
+};
+
+let obj2 = {
+    c: '3',
+    d: '4'
+};
+
+result = {...obj1, ...obj2};
+console.log(result) // {a: '1', b: '2', c: '3', d: '4'}
